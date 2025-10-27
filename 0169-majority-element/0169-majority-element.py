f6c01799f7d9818fe25095b58a4f3566b5884cff
Manthan VS <manthansankpal745@gmail.com>
2025-10-27 22:28:58 +0530

    Time: 4 ms (71.12%), Space: 19.2 MB (97.7%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,11 +1,14 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        count = 0
+        votes = 0
         candidate = None
 
         for num in nums:
-            if count == 0:
+            if votes == 0:
                 candidate = num
-            count += (1 if num == candidate else -1)
+
+            # Using the Python ternary conditional expression/one-line if-else: 
+            # value_if_true if condition else value_if_false
+            votes = votes + (1 if num == candidate else -1)
 
         return candidate
