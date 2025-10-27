58e05e01f740716192f09ec4fc4d9f51bde24083
Manthan VS <manthansankpal745@gmail.com>
2025-10-27 23:19:02 +0530

    Time: 1 ms (86.73%), Space: 19.4 MB (19.03%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,6 +1,6 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        # Using the Moore's voting algorithm here to effectively use the O(n) complexity. 
+        # Using the Moore's voting algorithm here to effectively use the O(1) complexity. 
         votes = 0
         candidate = None
 
