ccbfc3511f0a0a2ce71fbf3834efef87adfadbaf
Manthan VS <manthansankpal745@gmail.com>
2025-11-01 21:05:18 +0530

    Time: 107 ms (42.16%), Space: 27 MB (8.63%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,22 +1,30 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        # Initializing two pointers: 'buy' (buying day) and 'sell' (selling day).
         buy, sell = 0, 1
 
-        # Would keep a check of the prices where the profit concluded by the prices[sell] - prices[buy] is at maximum.
+        # maxProfit would be the Variable to store the maximum profit that can be achieved.
         maxProfit = 0
 
-        while sell < len(prices):  # As in when the array elements have traversed completely.
-            # For a profitable transaction.
-            if prices[buy] < prices[sell]:  # a profit basic condition
+        # Here, the loop runs until the 'sell' pointer reaches the end of the price list. 
+        while sell < len(prices):
+            
+            # Profit condition: We check here, if the current selling price is greater than the buying price i,e. buy < dell — a profitable condition. 
+            if prices[buy] < prices[sell]:
+                
+                # Calculate profit for the current buy-sell pair.
                 profit = prices[sell] - prices[buy]
-                maxProfit = max(maxProfit, profit)  # Whichever profit is maximum, let it be current ($profit) or previous ($maxProfit) store it in $maxProfit.
-
-            # For a loss transaction.
+                
+                # Update 'maxProfit' with the higher value between current($profti which we calculated just now) and previous profits($maxProft which is stored previously).
+                maxProfit = max(maxProfit, profit)
+            
+            # Loss condition: If the selling price is less than or equal to the buying price — it's a loss or no gain.
             else:
-                # As the selling price is not greater than the buying price. Therefor the selling price should be the new buying price.
+                # We move the 'buy' pointer to the 'sell' pointer position, as the new price could be a better buying opportunity.
                 buy = sell
             
-            # Regardless of our conditions the selling prices should be considered to be updated, to check on further days. 
-            sell += 1 
+            # Move the 'sell' pointer forward to check the next day's price.
+            sell += 1
         
+        # Returning the highest profit possible, if the sell pointer exceeds then by default it would give 0.
         return maxProfit
