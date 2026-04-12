8484ecfa057cadd7c520d4e6c76f7b5dc9c341ef
Manthan VS <manthansankpal745@gmail.com>
2026-04-12 23:24:28 +0530

    Time: 0 ms (100%), Space: 42.9 MB (82.27%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.java
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -1,6 +1,5 @@
 class Solution {
-    public String mergeAlternately(String word1, String word2) 
-    {
+    public String mergeAlternately(String word1, String word2) {
         char w1[] = word1.toCharArray();
         char w2[] = word2.toCharArray();
         int len1 = w1.length;
@@ -8,27 +7,17 @@ class Solution {
         char answer[] = new char[len1 + len2];
         int index = 0;
         int idx = 0;
-        for (int i = 0; i < answer.length; i++) 
-        {
-            if (i % 2 == 0) 
-            {
-                if (index < len1) 
-                {
+        for (int i = 0; i < answer.length; i++) {
+            if (i % 2 == 0) {
+                if (index < len1) {
                     answer[i] = w1[index++];
-                } 
-                else 
-                {
+                } else {
                     answer[i] = w2[idx++];
                 }
-            } 
-            else 
-            {
-                if (idx < len2) 
-                {
+            } else {
+                if (idx < len2) {
                     answer[i] = w2[idx++];
-                } 
-                else 
-                {
+                } else {
                     answer[i] = w1[index++];
                 }
             }
