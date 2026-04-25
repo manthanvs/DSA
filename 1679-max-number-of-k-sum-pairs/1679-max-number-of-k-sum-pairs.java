e60004cfb876eb92b2a3cf4d825cea761531eb2f
Manthan VS <manthansankpal745@gmail.com>
2026-04-25 10:15:51 +0530

    Time: 19 ms (99.76%), Space: 69.1 MB (71.33%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.java
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.java
@@ -1,25 +1,24 @@
 class Solution {
     public int maxOperations(int[] nums, int k) {
-        // Sort the array to use two-pointer technique
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] >= k) nums[i] = 0;
+        }
+
         Arrays.sort(nums);
+        int left = 0, right = nums.length - 1;
         int count = 0;
-        int i = 0;
-        int j = nums.length - 1;
-
-        while (i < j) {
-            int sum = nums[i] + nums[j];
-
-            if (sum == k) {
-                // Found a valid pair
-                count++;
-                i++;
-                j--;
-            } else if (sum > k) {
-                // Too big, move the end pointer
-                j--;
-            } else {
-                // Too small, move the start pointer
-                i++;
+        while(left < right){
+            int sum = nums[left] + nums[right];
+            if(sum == k){
+                count += 1;
+                left++;
+                right--;
+            }
+            else if(sum > k){
+                right--;
+            }
+            else {
+                left++;
             }
         }
 
