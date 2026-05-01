72c77669951264ae8e244fd90eda083c4486443c
Manthan VS <manthansankpal745@gmail.com>
2026-05-01 18:31:06 +0530

    Time: 104 ms (99.24%), Space: 30.9 MB (88.11%) - LeetHub

--- 0396-rotate-function/0396-rotate-function.py
+++ 0396-rotate-function/0396-rotate-function.py
@@ -1,17 +1,13 @@
 class Solution:
-    def maxRotateFunction(self, A: List[int]) -> int:
-        a_sum = 0
-        F = 0
-        n = len(A)
-
-        for i in range(n):
-            a_sum += A[i]
-            F += i * A[i]
-
-        res = F
-
-        for i in range(1, n):
-            F += a_sum - n * A[-i]
-            res = max(res, F)
-
-        return res
+    def maxRotateFunction(self, nums: List[int]) -> int:
+        n = len(nums)
+        total_s = sum(nums)
+        rolling_sum = 0
+        best = 0
+        start = 0
+        for i in range(n-1,0,-1):
+            start += i*nums[i]
+            i_change = total_s - n*nums[i]
+            rolling_sum = rolling_sum + i_change
+            if rolling_sum > best: best = rolling_sum
+        return start + best 
