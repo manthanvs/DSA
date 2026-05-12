7b79e2d5346310defaa2c40114af5e677b643a0e
Manthan VS <manthansankpal745@gmail.com>
2026-05-12 12:51:14 +0530

    Time: 52 ms (60.4%), Space: 55.5 MB (60.4%) - LeetHub

--- 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.py
+++ 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.py
@@ -1,18 +1,7 @@
 class Solution:
-    def minimumEffort(self, shop: List[List[int]]) -> int:
-        shop.sort(key=lambda x: x[1] - x[0], reverse=True)
-        
-        start = shop[0][1]
-        bal = shop[0][1] - shop[0][0]
-        loan = 0
-
-        for i in range(1, len(shop)):
-            cost, thresh = shop[i]
-            
-            if bal < thresh:
-                loan += thresh - bal
-                bal = thresh
-                
-            bal -= cost
-
-        return start + loan
+    def minimumEffort(self, tasks):
+        tasks.sort(key=lambda x: x[1] - x[0], reverse=True)
+        need = 0
+        for actual, minimum in reversed(tasks):
+            need = max(minimum, need + actual)
+        return need
