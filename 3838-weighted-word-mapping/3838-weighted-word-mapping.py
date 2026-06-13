eac1528ba9248c3767ec1906d383a2136f79a263
Manthan VS <manthansankpal745@gmail.com>
2026-06-14 03:03:11 +0530

    Time: 11 ms (47.2%), Space: 19.4 MB (34.27%) - LeetHub

--- 3838-weighted-word-mapping/3838-weighted-word-mapping.py
+++ 3838-weighted-word-mapping/3838-weighted-word-mapping.py
@@ -1,11 +1,21 @@
 class Solution:
-    def mapWordWeights(self, words: List[str], wt: List[int]) -> str:
-        res = []
-
-        for word in words:
-            s = 0
-            for ch in word:
-                s += wt[(ord(ch) & (1 << 5) - 1) - 1]
-            res.append(chr(122 - (s - ((s * 2521) >> (1 << 4)) * len(wt))))
-
-        return "".join(res)
+    def mapWordWeights(self, words: List[str], weights: List[int]) -> str:
+        alpha="abcdefghijklmnopqrstuvwxyz"
+        dict_={}
+        j=0
+        for i in alpha:
+            dict_[i]=weights[j]
+            j+=1
+        add=0
+        ans=[]
+        for i in words:
+            add=0
+            for j in i:
+                add=add+dict_[j]
+            result=add%26
+            ans.append(result)
+        answer=""
+        for i in ans:
+            answer=answer+chr(96+26-i)
+        return answer
+        
