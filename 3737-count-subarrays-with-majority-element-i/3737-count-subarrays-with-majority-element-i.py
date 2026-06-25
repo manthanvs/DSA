77e7a635061ee867aabbc6ab380ed60eabe3a6d6
Manthan VS <manthansankpal745@gmail.com>
2026-06-25 19:37:48 +0530

    Time: 9 ms (100%), Space: 19.5 MB (30.22%) - LeetHub

--- 3737-count-subarrays-with-majority-element-i/3737-count-subarrays-with-majority-element-i.py
+++ 3737-count-subarrays-with-majority-element-i/3737-count-subarrays-with-majority-element-i.py
@@ -1,18 +1,19 @@
 class Solution:
     def countMajoritySubarrays(self, nums: List[int], target: int) -> int:
         n = len(nums)
-        ans = 0
-
-        for l in range(n):
-            target_count = 0
-
-            for r in range(l, n):
-                if nums[r] == target:
-                    target_count += 1
-
-                length = r - l + 1
-
-                if target_count > length // 2:
-                    ans += 1
-
-        return ans
+        t = target
+        cnt = [0] * (n*2+2)
+        acc = [0] * (n*2+2)
+        pre = n+1
+        cnt[pre]=1
+        acc[pre]=1
+        res =0
+        for num in nums:
+            if t==num:
+                pre+=1
+            else:
+                pre-=1
+            cnt[pre]+=1
+            acc[pre] = acc[pre-1] + cnt[pre]
+            res+=acc[pre-1]
+        return res
