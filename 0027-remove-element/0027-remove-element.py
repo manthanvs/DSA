08c1b39d2a3a2ef875a405eb8867489446cbbc59
Manthan VS <manthansankpal745@gmail.com>
2026-07-01 15:50:06 +0530

    Time: 0 ms (100%), Space: 19.3 MB (17.7%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -1,9 +1,8 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        k = 0
+        index = 0
         for i in range(len(nums)):
             if nums[i] != val:
-                nums[k] = nums[i]
-                # k as a counter exclluding the duplicates of val. 
-                k += 1
-        return k
+                nums[index]=nums[i]
+                index += 1
+        return index
