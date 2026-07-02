112538529fc1b583e1b92d80adeea50a6cfbd097
Manthan VS <manthansankpal745@gmail.com>
2026-07-02 12:28:59 +0530

    Time: 0 ms (100%), Space: 19.4 MB (79.05%) - LeetHub

--- 0682-baseball-game/0682-baseball-game.py
+++ 0682-baseball-game/0682-baseball-game.py
@@ -12,3 +12,23 @@ class Solution:
                 elif operations[i] == "C":
                     res.pop()  # pop is way quicker here than del
         return sum(res)
+        
+        
+# * For better understanding
+
+# operations = ["5","-2","4","C","D","9","+","+"]
+# res = []
+# for i in range(len(operations)):
+#     try:
+#         res.append(int(operations[i]))
+#         print(res)
+#     except ValueError:
+#         if operations[i] == "+":
+#             print(res[-2])
+#             res.append(res[-2] + res[-1])
+#         elif operations[i] == "D":
+#             res.append(res[-1] * 2)
+#         elif operations[i] == "C":
+#             res.pop()  # pop is way quicker here than del
+#         print("Except", res)
+# print(sum(res))
