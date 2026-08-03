f05152cbee47fc5fe98051890e90d7d6bf7c3715
Manthan VS <manthansankpal745@gmail.com>
2026-08-03 10:40:02 +0530

    Time: 98 ms (99.35%), Space: 23.9 MB (71.59%) - LeetHub

---
+++ 1406-stone-game-iii/1406-stone-game-iii.py
@@ -0,0 +1,16 @@
+class Solution:
+    def stoneGameIII(self, stoneValue: List[int]) -> str:        
+        s1, s2, s3 = 0, 0, 0
+        tot = 0
+
+        for value in reversed(stoneValue):
+            tot += value
+            s1, s2, s3 = tot-min(s1,s2,s3),s1,s2
+        bob = tot - s1
+        if s1 > bob:
+            return "Alice"
+        if s1 < bob:
+            return "Bob"
+        if s1 == bob:
+            return "Tie"
+        
