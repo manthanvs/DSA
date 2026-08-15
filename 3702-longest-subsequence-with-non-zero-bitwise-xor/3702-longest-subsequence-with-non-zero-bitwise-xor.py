67000e98bfe213f8631c108d3dd1d7a893d0bdc5
Manthan VS <manthansankpal745@gmail.com>
2026-08-15 18:14:41 +0530

    Time: 22 ms (85.48%), Space: 34 MB (34.68%) - LeetHub

---
+++ 3702-longest-subsequence-with-non-zero-bitwise-xor/3702-longest-subsequence-with-non-zero-bitwise-xor.py
@@ -0,0 +1,10 @@
+class Solution:
+    def longestSubsequence(self, nums: List[int]) -> int:
+        n=len(nums)
+        if [0]*(n)==nums:
+            return 0
+        x=0
+        for i in nums:
+            x^=i
+        return n if x else n-1
+        
