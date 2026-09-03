087e13b43473af0786f0adcdbe28ae7562dbf991
Manthan VS <manthansankpal745@gmail.com>
2026-09-03 16:46:10 +0530

    Time: 15 ms (85.98%), Space: 35.5 MB (28.66%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.py
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.py
@@ -1,3 +1,9 @@
 class Solution:
-    def uniformArray(self, A: list[int]) -> bool:
-        return not (min(A) ^ reduce(or_, A)) & 1
+    def uniformArray(self, nums1: list[int]) -> bool:
+        m=min(nums1)
+        if m&1:
+            return True
+        for n in nums1:
+            if n&1:
+                return False
+        return True
