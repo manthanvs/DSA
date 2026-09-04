0d8cce8c314156e9fd5b39c94d57df5862cef1d6
Manthan VS <manthansankpal745@gmail.com>
2026-09-04 11:50:17 +0530

    Time: 7 ms (48.03%), Space: 19.3 MB (71.43%) - LeetHub

--- 3903-smallest-stable-index-i/3903-smallest-stable-index-i.py
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.py
@@ -4,7 +4,8 @@ class Solution:
         vmax = [nums[0]] * n
         vmin = [nums[-1]] * n
         for i in range(1, n):
-            vmax[i] = max(vmax[i - 1], nums[i]); vmin[n - 1 - i] = min(vmin[n - i], nums[n - 1 - i])
+            vmax[i] = max(vmax[i - 1], nums[i])
+            vmin[n - 1 - i] = min(vmin[n - i], nums[n - 1 - i])
         for i in range(n):
             if vmax[i] - vmin[i] <= k: return i
         return -1
